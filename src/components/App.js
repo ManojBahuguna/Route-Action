c59df43b09ba15ed92700f25a8809b551f78156f
Manoj Bahuguna <manojbahugunamanoj@gmail.com>
2018-06-10 16:41:43 +0530

    fetch sample data from server and pass to users component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Users from './Users';
 
-const App = () => <div>Route Action</div>
+class App extends Component {
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    fetch('http://demo9197058.mockable.io/users')
+      .then(res => res.json())
+      .then(posts => {
+        this.setState({ posts });
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Users users={this.state.posts} />
+      </div>
+    );
+  }
+}
 
 export default App;
